9b6ffeae8f8ed7982340fe1bde79268ecab663a0
임준영 <limejuny@gmail.com>
2022-01-25 00:45:35 +0900

    Swagger, h2 database 추가
    Detail
    ======
     - SwaggerConfig: 버전 호환성 문제로 BeanPostProcessor 추가
       - https://stackoverflow.com/questions/70178343/springfox-3-0-0-is-not-working-with-spring-boot-2-6-0
     - h2: 테스트 전용
     - application.yml: local, dev로 분리

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,93 @@
+package com.example.demo.config;
+
+import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+
+import io.swagger.annotations.ApiOperation;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.OAS_30)
+      .useDefaultResponseMessages(false)
+      .consumes(getConsumeContentTypes())
+      .produces(getProduceContentTypes())
+      .apiInfo(apiInfo())
+      .select()
+      .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+      .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+      .paths(PathSelectors.any())
+      .build();
+  }
+
+  private Set<String> getConsumeContentTypes() {
+    Set<String> consumes = new HashSet<>();
+    consumes.add("application/json;charset=UTF-8");
+    return consumes;
+  }
+
+  private Set<String> getProduceContentTypes() {
+    Set<String> produces = new HashSet<>();
+    produces.add("application/json;charset=UTF-8");
+    return produces;
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+      .title("Kafka Demo")
+      .description("Kafka Pub/Sub Demo Project")
+      .version("1.0.0")
+      .build();
+  }
+
+  @Bean
+  public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+    return new BeanPostProcessor() {
+
+      @Override
+      public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof WebMvcRequestHandlerProvider ) {
+          customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+        }
+        return bean;
+      }
+
+      private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+        List<T> copy = mappings.stream()
+          .filter(mapping -> mapping.getPatternParser() == null)
+          .collect(Collectors.toList());
+        mappings.clear();
+        mappings.addAll(copy);
+      }
+
+      @SuppressWarnings("unchecked")
+      private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+        try {
+          Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+          field.setAccessible(true);
+          return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+          throw new IllegalStateException(e);
+        }
+      }
+    };
+  }
+}
